953939324c562fed1d0c76b98a5af9757356ca3d
Danna López <124531588+Tysh4L@users.noreply.github.com>
2023-02-21 23:35:36 -0600

    Add files via upload

---
+++ EVA1_9_STACKOVERFLOW/src/eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,19 @@
+package eva1_9_stackoverflow;
+
+/**
+ *
+ * @author danna
+ */
+public class EVA1_9_STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    public static void A(){
+        A();
+    }
+}
