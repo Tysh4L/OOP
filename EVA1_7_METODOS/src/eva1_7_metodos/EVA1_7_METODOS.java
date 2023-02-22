bdf416dfd58cb23b8f4986eca1ba9f2ffe403d2b
Danna López <124531588+Tysh4L@users.noreply.github.com>
2023-02-21 23:34:07 -0600

    Add files via upload

---
+++ EVA1_7_METODOS/src/eva1_7_metodos/EVA1_7_METODOS.java
@@ -0,0 +1,19 @@
+
+package eva1_7_metodos;
+
+/**
+ *
+ * @author danna
+ */
+public class EVA1_7_METODOS {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        myMethod();//invocar al metodo: llamada a funcion
+    }
+    public static void myMethod(){
+        System.out.println("You have called me: my name is myMethod!!");
+    }
+            
+}
