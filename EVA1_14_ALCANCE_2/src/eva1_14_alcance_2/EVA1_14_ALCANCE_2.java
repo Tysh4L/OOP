440391b6c2fd2e7ed3c96c7b2d257635bd2f1c85
Danna López <124531588+Tysh4L@users.noreply.github.com>
2023-02-24 09:03:30 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE_2/src/eva1_14_alcance_2/EVA1_14_ALCANCE_2.java
@@ -0,0 +1,25 @@
+package eva1_14_alcance_2;
+
+/**
+ *
+ * @author danna
+ */
+public class EVA1_14_ALCANCE_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 100;//visible dentro de todo el bloque main();
+        for (int i = 0; i < 10; i++) {
+            int x = 100;//la variable declarada en main()y
+                        //es visible dentro del for
+            int y = 100;//visivle dentro del for
+            if (i == 5);{
+        }
+    }
+    int y = 100;//aqui la variable y del ciclo for no existe
+                //podemos declarar una variable del mismo nombre
+  }
+}
