4337a31ee0cd2095aa55844cb183561a06b52edc
Danna López <124531588+Tysh4L@users.noreply.github.com>
2023-02-24 09:04:17 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,23 @@
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author danna
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*DIFERENTES PRINTLN: 10 DIFERENTES
+        METODOS PARA IMPRIMIR DIFERENTES TIPOS DE DATOOS
+        LA FIRMA DEL METODO DEBE SER UNICA*/
+        System.out.println("Hola");
+        System.out.println("1212");
+        System.out.println(true);
+        System.out.println(args);
+    }
+    
+}
