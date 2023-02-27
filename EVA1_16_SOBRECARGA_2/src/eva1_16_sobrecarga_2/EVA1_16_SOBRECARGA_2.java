9db793438bb519486faf747780941b857fc1702e
Danna López <124531588+Tysh4L@users.noreply.github.com>
2023-02-26 22:42:29 -0600

    Add files via upload

---
+++ EVA1_16_SOBRECARGA_2/src/eva1_16_sobrecarga_2/EVA1_16_SOBRECARGA_2.java
@@ -0,0 +1,36 @@
+package eva1_16_sobrecarga_2;
+
+/**
+ *
+ * @author danna
+ */
+public class EVA1_16_SOBRECARGA_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Suma: " + suma(4,5));
+        System.out.println("Suma: "+ suma(4.0, 5.0));
+        System.out.println("Suma: "+ suma("Hola ","mundo!"));
+        suma();
+    }
+                        //suma(int, int)
+    public static int suma(int val1, int val2){
+        return val1 + val2;
+    }
+                        //suma(double, double)
+    public static double suma(double val1, double val2){
+        return val1 + val2;
+    }
+                        //suma(String, String)
+    public static String suma(String val1, String val2){
+        return val1 + val2;
+    }
+                      //suma()
+    public static void suma(){
+        System.out.println("Metodo sin parametros suma");
+    }
+    
+}
